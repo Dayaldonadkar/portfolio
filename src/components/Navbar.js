1c72f0ced3c6ab2013fadaff7fbd57bed2b0e364
Dayaldonadkar <dayalpython@gmail.com>
2023-11-12 21:04:03 +0530

    m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,15 +13,17 @@ const Navbar = () => {
       <div className="flex justify-between w-[90%] py-3 relative">
         <div className="flex justify-between w-[100%] space-x-5">
           <h1>Dayal</h1>
-          <div className="flex w-[80%] space-x-5 justify-end  lg:space-x-5 font-montserrat text-[#707071]">
-            <p>Home</p>
-            <p className="">About</p>
-            <p className="">What I do</p>
-            <p className="">Skills</p>
-            <p className="">Experience</p>
-            <p className="">Projects</p>
-            <p className="">Portfolio</p>
-            <p>Contact</p>
+          <div className="hidden lg:block">
+            <div className="flex w-[80%] space-x-5 justify-end  lg:space-x-5 font-montserrat text-[#707071] ">
+              <p>Home</p>
+              <p className="">About</p>
+              <p className="">What I do</p>
+              <p className="">Skills</p>
+              <p className="">Experience</p>
+              <p className="">Projects</p>
+              <p className="">Portfolio</p>
+              <p>Contact</p>
+            </div>
           </div>
         </div>
         <div onClick={() => handleChange()} className="md:hidden">
@@ -31,7 +33,7 @@ const Navbar = () => {
 
       {showMenu && (
         <div className="absolute top-10 bg-[#F8F9FA] w-[100%] flex justify-center md:hidden">
-          <div className="space-y-2 py-3">
+          <div className="space-y-2 py-3 font-montserrat">
             <p>Home</p>
             <p>About</p>
             <p>What I do</p>
