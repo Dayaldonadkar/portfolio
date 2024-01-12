f23543ca7543dac55844031203e83942c36eea84
Dayal Sudhakar <Dayalpython@gmail.com>
2024-01-13 04:19:57 +0530

    logos added

--- src/components/TechnologyUsed.js
+++ src/components/TechnologyUsed.js
@@ -3,6 +3,8 @@ import react from "../Assets/1174949_js_react js_logo_react_react native_icon.pn
 import taiwind from "../Assets/9055799_bxl_tailwind_css_icon.png";
 import java from "../Assets/282802_javascript_js_icon.png";
 import redux from "../Assets/4691205_redux_icon.png";
+import html from "../Assets/html logo.png";
+import css from "../Assets/csslogo.png";
 
 const TechnologyUsed = () => {
   return (
@@ -10,28 +12,42 @@ const TechnologyUsed = () => {
       <div className="flex flex-col justify-center items-center w-[90%]">
         <h1 className=" font-semibold text-4xl pb-4">Skills</h1>
         <div className="flex justify-center">
-          <div className="grid grid-cols-4 place-items-center space-x-5 sm:space-x-8 pt-5">
+          <div className="grid grid-cols-3 place-items-center gap-x-10 gap-y-6 md:gap-y-10  pt-5">
             <div className=" border-4 border-blue-500 rounded-full">
-              <img className="w-8 md:w-10 m-2 sm:m-5" src={react} alt="" />
+              <img className="w-10 md:w-14 m-4 sm:m-6" src={react} alt="" />
             </div>
             <div className=" border-4 border-blue-500 rounded-full">
               <img
-                className="w-8 md:w-10 m-2 sm:m-5"
+                className="w-10 md:w-16 m-4 sm:m-6"
                 src={taiwind}
                 alt="tailwind logo"
               />
             </div>
             <div className=" border-4 border-blue-500 rounded-full">
               <img
-                className="w-8 md:w-10  m-2 sm:m-5"
+                className="w-10 md:w-16  m-4 sm:m-6"
                 src={java}
                 alt="javascript logo"
               />
             </div>
             <div className=" border-4 border-blue-500 rounded-full">
               <img
-                className="w-8 md:w-10  m-2 sm:m-5"
+                className="w-10 md:w-16  m-4 sm:m-6"
                 src={redux}
+                alt="redux logo"
+              />
+            </div>
+            <div className="border-4 border-blue-500 rounded-full">
+              <img
+                className="w-10 md:w-16  m-4 sm:m-6"
+                src={html}
+                alt="redux logo"
+              />
+            </div>
+            <div className="border-4 border-blue-500 rounded-full">
+              <img
+                className="w-10 md:w-16  m-4 sm:m-6"
+                src={css}
                 alt="redux logo"
               />
             </div>
